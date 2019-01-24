ded10052cc637d64fdc28e9e4678eaa0a8214c53
BoCheng <jokerlin08666@gmail.com>
2019-01-24 15:49:33 +0800

    FirstCommit

--- Live broadcast_Project/ViewController.swift
+++ Live broadcast_Project/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
 
 class ViewController: UIViewController {
 
@@ -14,7 +16,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+  
+    @IBAction func LoginButton(_ sender: UIButton) {
+        let manger = LoginManager()
+        manger.logIn(readPermissions: [.publicProfile], viewController: self) { (result) in
+            if let accessToken = AccessToken.current {
+                print("access Token :\(accessToken)")
+            }
+            switch result {
+            case .cancelled:
+                print("cancel")
+            case .failed(let error):
+                print(error.localizedDescription)
+            case .success(grantedPermissions: let grantedPermissions , declinedPermissions: let declinedPermissions, token: let token):
+                print(grantedPermissions)
+                print(declinedPermissions)
+                print(token)
+                print("Login in !!!!")
+            }
+            
+        }
+    }
+        
 }
+    
+
+
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Live broadcast_Project' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Live broadcast_Project
+	pod 'FacebookCore'
+	pod 'FacebookLogin'
+	pod 'FacebookShare'
+end
