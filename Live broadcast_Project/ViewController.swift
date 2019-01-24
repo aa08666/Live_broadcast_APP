055efae48c16215a7b05dfdfadc5e50da1690f6a
BoCheng <jokerlin08666@gmail.com>
2019-01-24 10:40:00 +0800

    Initial Commit

---
+++ Live broadcast_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Live broadcast_Project
+//
+//  Created by 柏呈 on 2019/1/24.
+//  Copyright © 2019 柏呈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
